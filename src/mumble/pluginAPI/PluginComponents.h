7ffb249f904e7ed9d9d1436450a86741deecd108
Robert Adam <dev@robert-adam.de>
2019-08-13 19:51:06 +0200

    put custom types and the like in own file

---
+++ src/mumble/pluginAPI/PluginComponents.h
@@ -0,0 +1,18 @@
+/**
+ * This header file contains definitions of types and other components used in Mumble's plugin system
+ */
+
+#ifndef MUMBLE_PLUGINCOMPONENT_H_
+#define MUMBLE_PLUGINCOMPONENT_H_
+
+// Define an enum for error codes
+enum Status {
+	OK=0,
+	ERROR
+};
+
+struct MumbleFunctions {
+};
+
+
+#endif
