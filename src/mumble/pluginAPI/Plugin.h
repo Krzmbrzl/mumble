ae5878e2dbda92b41580b2eb148d45c0d66304e4
Robert Adam <dev@robert-adam.de>
2019-08-13 19:50:23 +0200

    added some API functions; changed others; removed namespace from API functions

--- src/mumble/pluginAPI/Plugin.h
+++ src/mumble/pluginAPI/Plugin.h
@@ -5,37 +5,38 @@
 #ifndef MUMBLE_PLUGIN_H_
 #define MUMBLE_PLUGIN_H_
 
+#include "PluginComponents.h"
+
 #ifdef WIN32
 #define PLUGIN_EXPORT __declspec(dllexport)
 #else
 #define PLUGIN_EXPORT __attribute__ ((visibility("default")))
 #endif
 
-namespace MumblePlugin {
-
-	// Define an enum for error codes
-	enum Status {
-		OK=0,
-		ERROR
-	};
-
 #ifdef __cplusplus
-	extern "C" {
+extern "C" {
 #endif
 
-		// functions for init and de-init
-		PLUGIN_EXPORT Status init();
-		PLUGIN_EXPORT Status shutdown();
+	// functions for init and de-init
+	PLUGIN_EXPORT Status init();
+	PLUGIN_EXPORT void shutdown();
 
-		// functions for general plugin info
-		PLUGIN_EXPORT const char* getName();
-		PLUGIN_EXPORT const char* getVersion();
-		PLUGIN_EXPORT const char* getAuthor();
+	// functions for general plugin info
+	PLUGIN_EXPORT const char* name();
+	PLUGIN_EXPORT const char* version();
+	PLUGIN_EXPORT int apiVersion();
+	PLUGIN_EXPORT const char* author();
+	PLUGIN_EXPORT const char* description();
+	PLUGIN_EXPORT void setFunctionPointers(const struct MumbleFunctions functions);
+
+
+
+	PLUGIN_EXPORT void registerPluginID(const char* id);
+
 
 #ifdef __cplusplus
-	}
+}
 #endif 
 
-} // MumblePlugin
 
 #endif
