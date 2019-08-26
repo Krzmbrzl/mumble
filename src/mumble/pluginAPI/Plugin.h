5fa54309dd68f3d3fab4f5024bc5d8a74389b92f
Robert Adam <dev@robert-adam.de>
2019-08-26 16:09:17 +0200

    Further work on the API

--- src/mumble/pluginAPI/Plugin.h
+++ src/mumble/pluginAPI/Plugin.h
@@ -21,11 +21,15 @@ extern "C" {
 	PLUGIN_EXPORT Status init();
 	PLUGIN_EXPORT void shutdown();
 
+	// The intention is to call this function as a first step whenever loading a plugin - It's good habit
+	// to tell your partner who you are first, right?
+	PLUGIN_EXPORT void setMumbleInfo(Version MumbleVersion, Version mumbleAPIVersion, Version minimalExpectedAPIVersion);
+
 	// functions for general plugin info
 	PLUGIN_EXPORT const char* name();
-	PLUGIN_EXPORT const char* getVersion();
-	PLUGIN_EXPORT int getNumericVersion(); // This could facilitate programmatic version comparisons
-	PLUGIN_EXPORT int getRequiredApiVersion();
+	PLUGIN_EXPORT const char* getDisplayVersion();
+	PLUGIN_EXPORT Version getVersion();
+	PLUGIN_EXPORT Version getRequiredApiVersion();
 	PLUGIN_EXPORT const char* getAuthor();
 	PLUGIN_EXPORT const char* getDescription();
 	PLUGIN_EXPORT void setFunctionPointers(const struct MumbleFunctions functions);
@@ -38,18 +42,29 @@ extern "C" {
 
 	// Parameters to functions below are yet to be determined
 
-	// Callback functions
+	// -------- Callback functions -----------
+
+	/// Called whenever the user changes his/her channel.
 	PLUGIN_EXPORT void onChannelChanged();
-	PLUGIN_EXPORT void onConnectedServerChanged();
+	/// Called when connecting to a server.
 	PLUGIN_EXPORT void onServerConnect();
+	/// Called when disconnecting from a server.
 	PLUGIN_EXPORT void onServerDisconnect();
+	/// Called when a new client connects to the server the user is currently connected to.
 	PLUGIN_EXPORT void onNewClientConnectedToServer();
+	/// Called when a client disconnects from the server the user is currently connected to.
 	PLUGIN_EXPORT void onClientDisconnectedFromServer();
+	/// Called when a client joins the channel the user is currently in.
 	PLUGIN_EXPORT void onClientJoinedChannel();
+	/// Called when a client leaves the channel the user is currently in.
 	PLUGIN_EXPORT void onClientLeftChannel();
+	/// Called when the user changes his/her username
 	PLUGIN_EXPORT void onUsernameChanged();
+	/// Called whenever there is audio input
 	PLUGIN_EXPORT void onAudioInput(short* inputPCM, bool isSpeech);
+	/// Called whenever there is audio output
 	PLUGIN_EXPORT void onAudioOutput_short(short* outputPCM, int sampleCount, int channelCount);
+	/// Called whenever there is audio output
 	PLUGIN_EXPORT void onAudioOutput_float(float* outputPCM, int sampleCount, int channelCount);
 
 
