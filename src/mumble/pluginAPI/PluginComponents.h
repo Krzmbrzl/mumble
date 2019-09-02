dcf9f08383106b9a88821599b8eeb9a1d0d48a43
Robert Adam <dev@robert-adam.de>
2019-09-02 19:20:57 +0200

    added serverConnection to the API in case mumble will support multiple connections at some point

--- src/mumble/pluginAPI/PluginComponents.h
+++ src/mumble/pluginAPI/PluginComponents.h
@@ -31,6 +31,8 @@ typedef struct Version Version_t;
 typedef struct MumbleUser MumbleUser_t;
 typedef struct MumbleChannel MumbleChannel_t;
 
+typedef int32_t MumbleConnection_t
+
 
 struct MumbleAPI {
 	// -------- Memory (de-)allocation --------
@@ -63,35 +65,46 @@ struct MumbleAPI {
 	
 	// -------- Getter functions --------
 
+	/// Fills in the connection ID of the server the user is currently active on (the user's audio output is directed at).
+	///
+	/// @param[out] connection A pointer to the memory location the ID should be written to
+	/// @returns The error code. If everything went well, STATUS_OK will be returned. Only then it is valid to access the
+	/// 	value of the provided pointer
+	uint32_t getActiveServerConnection(MumbleConnection_t *connection);
+
 	/// Fills in the information about the local user.
 	///
+	/// @param connection A reference to which server-connection should be considered.
 	/// @param[out] user The user struct allocated via allocateMumbleUser whose fields will be set by this function.
 	/// @returns The error code. If everything went well, STATUS_OK will be returned. Only then the fields of the
 	/// 	passed struct may be accessed.
-	uint32_t (*getLocalUser)(MumbleUser_t *user);
+	uint32_t (*getLocalUser)(MumbleConnection_t connection, MumbleUser_t *user);
 
 	/// Fills in the information about the user with the provided ID, if such a user exists.
 	///
 	/// @param userID The respective user's ID
+	/// @param connection A reference to which server-connection should be considered.
 	/// @param[out] user The user struct allocated via allocateMumbleUser whose fields will be set by this function.
 	/// @returns The error code. If everything went well, STATUS_OK will be returned. Only then the fields of the
 	/// 	passed struct may be accessed.
-	uint32_t (*getUser)(uint32_t userID, MumbleUser_t *user);
+	uint32_t (*getUser)(MumbleConnection_t connection, uint32_t userID, MumbleUser_t *user);
 
 	///	Fills in the information about the channel with the given ID, if such a channel exists.
 	///
+	/// @param connection A reference to which server-connection should be considered.
 	/// @param channelID The respective channel's ID
 	/// @param[out] channel The channel struct allocated via allocateMumbleChannel whose fields will be set by this function.
 	/// @returns The error code. If everything went well, STATUS_OK will be returned. Only then the fields of the passed
 	/// 	struct may be accessed.
-	uint32_t (*getChannel)(int32_t channelID, MumbleChannel_t *channel);
+	uint32_t (*getChannel)(MumbleConnection_t connection, int32_t channelID, MumbleChannel_t *channel);
 
 	/// Fills in the amount of clients that are currently connected to the server.
 	///
+	/// @param connection A reference to which server-connection should be considered.
 	/// @param[out] A pointer to the memory the amount shall be written
 	/// @returns The error code. If everything went well, STATUS_OK will be returned. Only then it is valid to access the
 	/// 	value of the provided pointer.
-	uint32_t (*getAmountOfConnectedClients)(size_t *amount);
+	uint32_t (*getAmountOfConnectedClients)(MumbleConnection_t connection, size_t *amount);
 
 
 
@@ -99,19 +112,21 @@ struct MumbleAPI {
 	
 	/// Fills in the information about a user with the specified name, if such a user exists.
 	///
+	/// @param connection A reference to which server-connection should be considered.
 	/// @param userName The respective user's name
 	/// @param[out] user The user struct allocated via allocateMumbleUser whose fields will be set by this function
 	/// @returns The error code. If everything went well, STATUS_OK will be returned. Only then the fields of the passed
 	/// 	struct may be accessed.
-	uint32_t (*findUserByName)(const char *userName, MumbleUser_t *user);
+	uint32_t (*findUserByName)(MumbleConnection_t connection, const char *userName, MumbleUser_t *user);
 
 	/// Fills in the information about a channel with the specified name, if such a channel exists.
 	///
+	/// @param connection A reference to which server-connection should be considered.
 	/// @param channelName The respective channel's name
 	/// @param[out] channel The channel struct allocated via allocateMumbleChannel whose fields will be set by this function
 	/// @returns The error code. If everything went well, STATUS_OK will be returned. Only then the fields of the passed
 	/// 	struct may be accessed.
-	uint32_t (*findChannelByName)(const char *channelName, MumbleChannel_t *channel);
+	uint32_t (*findChannelByName)(MumbleConnection_t connection, const char *channelName, MumbleChannel_t *channel);
 };
 
 
