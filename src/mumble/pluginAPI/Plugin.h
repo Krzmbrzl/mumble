70a9eef6fc8a5a12a854f0565b394cece704efa7
Robert Adam <dev@robert-adam.de>
2019-09-08 18:55:40 +0200

    added function for fetching positional data

--- src/mumble/pluginAPI/Plugin.h
+++ src/mumble/pluginAPI/Plugin.h
@@ -106,6 +106,34 @@ extern "C" {
 	/// 	FEATURE_NONE. If none of the requested features can be disabled return the unmodified features parameter.
 	PLUGIN_EXPORT uint32_t deactivateFeatures(uint32_t features);
 
+	
+	// -------- Positional Audio --------
+
+	/// Retrieves the positional audio data. If no data can be fetched, set all float-vectors to 0 and return false.
+	///
+	/// @param[out] avatar_pos A float-array of size 3 representing the cartesian position of the player/avatar in the ingame world.
+	/// 	One unit represents one meter of distance.
+	/// @param[out] avatar_front A float-array of size 3 representing the cartesian direction-vector of the player/avatar ingame (where it
+	/// 	is facing). One unit represents one meter of distance.
+	/// @param[out] avatar_top A float-array of size 3 representing <TODO>.One unit represents one meter of distance.
+	/// @param[out] camera_pos A float-array of size 3 representing the cartesian position of the camera in the ingame world.
+	/// 	One unit represents one meter of distance.
+	/// @param[out] camera_front A float-array of size 3 representing the cartesian direction-vector of the camera ingame (where it
+	/// 	is facing). One unit represents one meter of distance.
+	/// @param[out] camera_top A float-array of size 3 representing <TODO>.One unit represents one meter of distance.
+	/// @param[out] context A pointer to a C-encoded string storing the context of the provided positional data. This context should
+	/// 	include information about the server (and team) the player is on. Only players with identical context will be able to hear
+	/// 	each other's audio. The plugin has to guarantee that the returned pointer will be valid until freePositionalData has been called.
+	/// @param[out] identity A pointer to a C-encoded string storing the identity of the player. It can be polled by external scripts from the
+	/// 	server and should uniquely identify the player in the game. The plugin has to guarantee that the returned pointer will be valid
+	/// 	until freePositionalData has been called.
+	/// @returns Whether the data was successfully fetched
+	PLUGIN_EXPORT bool getPositionalData(float *avatar_pos, float *avatar_front, float *avatar_top, float *camera_pos, float *camera_front,
+			float *camera_top, const char *context, const char *identity);
+
+	/// A function indicating that the context and identity string allocated for getPositionalData may be freed from now on.
+	PLUGIN_EXPORT void freePositionalData();
+
 
 	// Parameters to functions below are yet to be determined
 
